918ffedd9c46ee1f77f4f348509bf5d0e57cb64c
John Xu <john.xu@ashbury.ca>
2018-04-11 11:25:12 -0400

    set size

--- src/Backgammon.java
+++ src/Backgammon.java
@@ -6,6 +6,7 @@ public class Backgammon {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Board bb = new Board();
         frame.getContentPane().add(bb);
+        bb.setSize(800,600);
 
         frame.pack();
         frame.setVisible(true);
--- src/Board.java
+++ src/Board.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
 
 public class Board extends JPanel {
+
 }
