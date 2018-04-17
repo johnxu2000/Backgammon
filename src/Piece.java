73d46007655f28e55555781ed7759608fb198d03
John Xu <john.xu@ashbury.ca>
2018-04-17 13:58:52 -0400

    Expanded the piece class

--- src/Piece.java
+++ src/Piece.java
@@ -1,7 +1,25 @@
 import java.awt.*;
 
 public class Piece {
-    public Piece(Color colour, int location){
-        
+    private Color colour;
+    private int width;
+    private int height;
+    private int x;
+    private int y;
+    private boolean canMove;
+    public Piece(Color colour, int width, int height, int x, int y, boolean canMove) {
+        this.colour = colour;
+        this.width = width;
+        this.height = height;
+        this.x = x;
+        this.y = y;
+        this.canMove = canMove;
+    }
+    public void draw(Graphics page){
+        page.setColor(colour);
+        page.drawOval(width,height,x,y);
+    }
+    public Color getColour(){
+        return colour;
     }
 }
