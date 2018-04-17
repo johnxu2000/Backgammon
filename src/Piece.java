a4b348878a7642fb9345c07da5754e08f696ca1b
John Xu <john.xu@ashbury.ca>
2018-04-17 15:10:48 -0400

    Added getter and setters for Piece class

--- src/Piece.java
+++ src/Piece.java
@@ -13,4 +13,22 @@ public class Piece {
         g.setColor(colour);
         g.fillOval(x,y,30,30);
     }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+    public void setY(int y){
+        this.y = y;
+    }
+
+    public int getX(){
+        return x;
+    }
+    public int getY(){
+        return y;
+    }
+
+    public Color getColour() {
+        return colour;
+    }
 }
