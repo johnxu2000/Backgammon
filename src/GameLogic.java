b12535f9de7e33675bd24be7a81780d3a1af4703
John Xu <john.xu@ashbury.ca>
2018-04-24 12:41:52 -0400

    started checkboard

--- src/GameLogic.java
+++ src/GameLogic.java
@@ -2,6 +2,7 @@ import java.util.Stack;
 
 public class GameLogic {
     Stack[] stacks = new Stack[24];
+    Boolean victory = false;
     public GameLogic() {
         for (int i = 0; i < 24; i++) {
             stacks[i] = new Stack();
@@ -9,6 +10,14 @@ public class GameLogic {
     }
     public Stack[] getStacks(){
             return stacks;
+    }
+    public void CheckPlayerPriority(){
+    }
+    public void CheckBoard(){
+        while(victory = false){
+            for (int i = 0; i > stacks.length; i++){
+            }
         }
+    }
 }
 
