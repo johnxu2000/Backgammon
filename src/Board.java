8331df296c74fc55b5a89b6ae62f71fab92f3eee
rakhavan <rakhavan@ashbury.ca>
2018-05-01 08:43:46 -0400

    I completed the move method

--- src/Board.java
+++ src/Board.java
@@ -138,6 +138,7 @@ public class Board extends JPanel {
                         removedPiece = (Piece) stacks.getStacks()[initialSpot].pop();
                         addPiece = (Piece) stacks.getStacks()[finalSpot].push(removedPiece);
                         moveStatus = true;
+                        turn = 1;
                         repaint();
                     }
                 }
@@ -150,6 +151,7 @@ public class Board extends JPanel {
                         moveStatus = true;
                         removedPiece = (Piece) stacks.getStacks()[initialSpot].pop();
                         addPiece = (Piece) stacks.getStacks()[finalSpot].push(removedPiece);
+                        turn = 2;
                         repaint();
                     }
                 }
